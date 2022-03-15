f745d46da0bfba383168305625c982e15f4c5989
Kyle MacPherson <kaimac2010@gmail.com>
2022-03-15 17:44:32 +0000

    Revert logic to send slack notification if a row has failed

--- lib/report_checker.rb
+++ lib/report_checker.rb
@@ -16,9 +16,8 @@ class ReportChecker
     failures_by_row = rows.map { |row| !row.include?("succeeded") || !row.include?("200") }
     any_rows_failed = failures_by_row.include?(true)
 
-    if any_rows_failed == false
+    if any_rows_failed == true
       webhookurl = ENV["SLACK_WEBHOOK_URL"]
-      p webhookurl
       notifier = Slack::Notifier.new(webhookurl, channel: "@macphersonkd", username: "notifier")
       notifier.ping "The redacted export has failures", channel: "@macphersonkd"
     end
